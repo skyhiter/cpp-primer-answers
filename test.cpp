5010bdfd0927f43febe50ce1e052dd54a2d4356c
skyhiter <guanzhu.luo@gmail.com>
2019-01-15 22:44:06 +0800

    add test.cpp for common test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int i = 2333;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << i << std::endl;
+    }
+    std::cout << i << std::endl;
+    return 0;
+}
